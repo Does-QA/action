7625298a085629a90a68abb9b81234a851a3a1e4
Sam Sm <sam@does.qa>
2026-03-23 11:18:35 +0000

    Security Fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const axios = require('axios');
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import axios from 'axios';
 
 const DOESQA_BASE = 'https://app.does.qa';
 const TERMINAL_STATUSES = [
